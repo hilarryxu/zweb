498e2574224fa63c47cafc88a7c13a77decaffd7
Larry Xu <hilarryxu@gmail.com>
2018-05-11 14:46:54 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zweb',
+    version='0.1.0',
+    description='zweb',
+    long_description='zweb',
+    author='Larry Xu',
+    author_email='hilarryxu@gmail.com',
+    url='http://www.hilarryxu.com/p/zweb',
+    packages=find_packages(exclude=('test*', 'docs', 'examples')),
+    include_package_data=True,
+    license='MIT',
+    zip_safe=False,
+    test_suite='tests'
+)
