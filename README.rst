a7c56b1a522bd62268cfe0f318e1eb4eed2daaa8
Larry Xu <hilarryxu@gmail.com>
2018-11-02 11:27:16 +0800

    Update setup.py

---
+++ README.rst
@@ -0,0 +1,24 @@
+zweb
+====
+
+A web framework for tornado with orm.
+
+
+Installing
+----------
+
+Install and update using `pip`_:
+
+.. code-block:: text
+
+    $ pip install git+https://github.com/hilarryxu/zweb.git#egg=zweb
+
+.. _pip: https://pip.pypa.io/en/stable/quickstart/
+
+
+Links
+-----
+
+*   License: `BSD <https://github.com/hilarryxu/zweb/blob/master/LICENSE>`_
+*   Code: https://github.com/hilarryxu/zweb
+*   Issue tracker: https://github.com/hilarryxu/zweb/issues
--- setup.py
+++ setup.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import io
+import re
 from setuptools import setup, find_packages
+
+kwargs = {}
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+with io.open('zweb/__init__.py', 'rt', encoding='utf8') as f:
+    version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+
+classes = """
+    Development Status :: 4 - Beta
+    Environment :: Web Environment
+    Framework :: Tornado
+    Intended Audience :: Developers
+    License :: OSI Approved :: BSD License
+    Operating System :: OS Independent
+    Programming Language :: Python
+    Programming Language :: Python :: 2
+    Programming Language :: Python :: 2.7
+    Programming Language :: Python :: Implementation :: CPython
+    Topic :: Internet :: WWW/HTTP
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
 
 setup(
     name='zweb',
-    version='0.1.1',
-    description='zweb',
-    long_description='zweb',
+    version=version,
+    description='A web framework for tornado',
+    long_description=readme,
     author='Larry Xu',
     author_email='hilarryxu@gmail.com',
-    url='http://www.hilarryxu.com/p/zweb',
-    packages=find_packages(exclude=('test*', 'docs', 'examples')),
+    url='https://github.com/hilarryxu/zweb',
+    license='BSD',
+    packages=find_packages(exclude=['test*', 'docs', 'examples']),
     include_package_data=True,
-    license='MIT',
-    zip_safe=False,
-    test_suite='tests'
+    classifiers=classifiers,
+    **kwargs
 )
